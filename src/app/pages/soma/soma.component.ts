554f6bc22e0402b2eedc02b5bb9db76f07d490e0
tiguassa <tiagofrancischini@gmail.com>
2025-04-22 00:33:27 -0300

    resolvendo conflitos do soma.componente.ts manualmente

--- src/app/pages/soma/soma.component.ts
+++ src/app/pages/soma/soma.component.ts
@@ -1,51 +1,26 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { SomaService } from '../../services'; 
-import { NumberSymbol } from '@angular/common';
-
-
-
-
 
 @Component({
   selector: 'app-soma',
   standalone: true,
   imports: [],
   templateUrl: './soma.component.html',
-
   styleUrls: ['./soma.component.css'] 
 })
 export class SomaComponent {
 
   private res: number = 0;
-
   constructor(private somaService: SomaService) {}
 
   somarBotao(numero1: string, numero2: string): void {
-    let n1 = parseFloat(numero1); // Corrigido: precisa usar "="
-    let n2 = parseFloat(numero2); // Corrigido também
+    let n1 = parseFloat(numero1);
+    let n2 = parseFloat(numero2);
     this.res = this.somaService.somar(n1, n2);
   }
 
   get resultado(): string {
-    return this.res.toString(); // Corrigido: era vírgula (,) errado, agora é ponto (.) para chamar função
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return this.res.toString();
 
   }
 }
